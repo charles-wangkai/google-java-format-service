653b5d4474431f58332ba8b6801d61e3df38de0c
Kai Wang <charles.wangkai@gmail.com>
2022-09-28 10:00:00 +0800

    Init

---
+++ src/main/java/com/example/googlejavaformatservice/GoogleJavaFormatServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.googlejavaformatservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleJavaFormatServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleJavaFormatServiceApplication.class, args);
+	}
+
+}
